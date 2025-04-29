6c8374cc47d394c21ee37fd5102a32ba87e1fe26
João Victor <jvs.devsilva@gmail.com>
2025-04-28 22:47:20 -0300

    Add webhook || url || enabled para recebimento dos eventos das conexões

--- src/main/java/com/zentry/whatsappapi/adapter/in/controller/instance/ConnectionController.java
+++ src/main/java/com/zentry/whatsappapi/adapter/in/controller/instance/ConnectionController.java
@@ -6,6 +6,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.core.type.TypeReference;
+
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -23,24 +25,29 @@ public class ConnectionController {
     }
 
     @PostMapping("/instance")
-    public  ResponseEntity<?> createInstance(@RequestBody Map<String, String> request) {
+    public  ResponseEntity<?> createInstance(@RequestBody Map<String, Object> request) {
 
-        String instanceName = request.get("instanceName");
-        Boolean qrcode = Boolean.valueOf(request.get("qrcode"));
-        String number = request.get("number");
-        String integration = request.get("integration");
-        String webhookUrl = request.get("webhookUrl");
-        String chain = request.get("chain");
+        Map<String, Object> webhook = (Map<String, Object>) request.get("webhook");
+        Boolean enabled = Boolean.valueOf((Boolean) webhook.get("enabled"));
+        String url = (String) webhook.get("url");
+        List<String> events = (List<String>) webhook.get("events");
+
+        // Pega os dados do JSON
+        String instanceName = (String) request.get("instanceName");
+        Boolean qrcode = Boolean.valueOf((Boolean) request.get("qrcode"));
+        String number = (String) request.get("number");
+        String integration = (String) request.get("integration");
+        String chain = (String) request.get("chain");
 
         System.out.println("Dados recebidos: number = " + number + ", text = " + qrcode);
 
-        if (number == null || chain == null || webhookUrl == null || integration == null) {
+        if (number == null || chain == null || webhook == null || integration == null) {
             return ResponseEntity.badRequest().body("Número, cadeia, URL do webhook e integração são obrigatórios.");
         }
 
         try {
-            String response = connectionService.createInstance(number, Boolean.valueOf(qrcode ? "true" : "false"), instanceName, integration, webhookUrl, chain);
-            Map<String, Object> responseMap = objectMapper.readValue(response, new TypeReference<Map<String, Object>>() {});
+            String response = connectionService.createInstance(number, Boolean.valueOf(qrcode ? "true" : "false"), instanceName, integration, chain, Boolean.valueOf(enabled ? "true" : "false"), url, events);
+            Map<String, Object> responseMap = objectMapper.readValue(response, new TypeReference<Map<String, Object>>() {}); // Converte a string JSON para um mapa
 
             return ResponseEntity.ok().body(Map.of(
                     "message", "Instância criada com sucesso!",
--- src/main/java/com/zentry/whatsappapi/application/service/connection/ConnectionService.java
+++ src/main/java/com/zentry/whatsappapi/application/service/connection/ConnectionService.java
@@ -1,41 +1,49 @@
 package com.zentry.whatsappapi.application.service.connection;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.*;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
 public class ConnectionService {
 
     private final RestTemplate restTemplate;
-    private final String API_URL = "http://localhost:8080//instance/create";
+    private final String API_URL = "http://evolution-api:8080/instance/create";
     private final String API_KEY = "12345";
 
     public ConnectionService(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
 
-    public String createInstance(String number, Boolean qrcode, String instanceName, String integration, String webhookUrl, String chain) {
+    public String createInstance(String number, Boolean qrcode, String instanceName, String integration, String chain, Boolean enabled, String url, List<String> events) {
+
+        Map<String, String> webHookMap = new HashMap<>();
+        webHookMap.put("enabled", String.valueOf(enabled));
+        webHookMap.put("url", url);
+        webHookMap.put("events", String.valueOf(events));
+
         // Monta o payload
         Map<String, Object> payload = new HashMap<>();
         payload.put("number", number);
         payload.put("qrcode", qrcode);
         payload.put("instanceName", instanceName);
         payload.put("integration", integration);
-        payload.put("webhookUrl", webhookUrl);
-        payload.put("chain", chain
-        );
+        payload.put("webhook", webHookMap);
+        payload.put("chain", chain);
 
         // Monta os headers
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.set("apikey", API_KEY);
 
-        HttpEntity<Map<String, Object>> request = new HttpEntity<>(payload, headers);
+        HttpEntity<Map<String, Object>> request = new HttpEntity<>(payload, headers); //
 
         try {
             ResponseEntity<String> response = restTemplate.postForEntity(API_URL, request, String.class);
