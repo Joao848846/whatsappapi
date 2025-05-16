d77fbef969affc71d180cec317ba1226bdaf35c8
João Victor <jvs.devsilva@gmail.com>
2025-05-16 10:29:41 -0300

    Adiciona métodos ao CsvService para buscar agendamentos por documento, telefone, nome e status de pagamento, além de listar todos os agendamentos

--- src/main/java/com/zentry/whatsappapi/application/service/scheduling/CsvService.java
+++ src/main/java/com/zentry/whatsappapi/application/service/scheduling/CsvService.java
@@ -6,8 +6,10 @@ import org.springframework.stereotype.Service;
 
 import java.io.InputStreamReader;
 import java.io.InputStream;
+import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.time.LocalTime;
+import java.text.NumberFormat;
+import java.util.Locale;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
@@ -44,24 +46,27 @@ public class CsvService {
                 agendamento.setDocumento(linha[2]);
                 agendamento.setTipo_contrato(linha[3]);
                 agendamento.setData_contrato(String.valueOf(LocalDate.parse(linha[4], dateFormatter)));
-                agendamento.setStatus_pagamento(linha[5]);
+                agendamento.setStatusPagamento(linha[5]);
                 agendamento.setValor_mensalidade(linha[6]);
                 agendamento.setLembreteEnviado(false);
                 agendamento.setDataUltimoLembreteEnviado(null);
 
                 // Verifica se já existe um agendamento com este documento
                 scheduling existente = agendamentoRepository.findByDocumento(agendamento.getDocumento());
+                scheduling existenteTelefone = agendamentoRepository.findByTelefone(agendamento.getTelefone());
 
-                if (existente == null) {
+                if (existente == null && existenteTelefone == null) {
                     // Se não existe, salva o novo agendamento e adiciona à lista
                     agendamentos.add(agendamento);
                     System.out.println("Agendamento adicionado: " + agendamento.getDocumento() + "UltimoLembrete: " + agendamento.getDataUltimoLembreteEnviado());
+                    System.out.println("Agendamento adicionado: " + agendamento.getTelefone() + "UltimoLembrete: " + agendamento.getDataUltimoLembreteEnviado());
                     agendamentoRepository.save(agendamento);
                     System.out.println("Agendamento salvo: " + agendamento.getDocumento() + "UltimoLembrete: " + agendamento.getDataUltimoLembreteEnviado());
 
                 } else {
                     // Se já existe, loga a duplicidade (ou outra ação que você preferir)
                     System.out.println("Duplicidade encontrada para o documento: " + agendamento.getDocumento());
+                    System.out.println("Duplicidade encontrada para o telefone: " + agendamento.getTelefone());
                     // Por enquanto, não adicionamos à lista 'agendamentos'
 
                 }
@@ -73,4 +78,29 @@ public class CsvService {
 
         return agendamentos;
     }
+
+    public List<scheduling> listarAgendamentos() {
+        return agendamentoRepository.findAll();
+    }
+
+    public scheduling buscarAgendamentoPorDocumento(String documento) {
+        return agendamentoRepository.findByDocumento(documento);
+    }
+
+    public scheduling buscarAgendamentoPorTelefone(String telefone) {
+        return agendamentoRepository.findByTelefone(telefone);
+    }
+
+    public scheduling buscarAgendamentoPorNome(String nome) {
+        return  agendamentoRepository.findByNome(nome);
+    }
+
+    public  List<scheduling> buscarAgendamentoPorStatusPagamento(String statusPagamento) {
+        return agendamentoRepository.findByStatusPagamento(statusPagamento); // Chamada com o nome correto
+    }
+
+    public List<scheduling> buscarPorLembreteEnviado(boolean lembreteEnviado) {
+        return agendamentoRepository.findByLembreteEnviado(lembreteEnviado);
+    }
+
 }
