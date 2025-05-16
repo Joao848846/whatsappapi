714b6148153867af8fbf779bb3f7d7bd0489b793
João Victor <jvs.devsilva@gmail.com>
2025-05-15 21:36:01 -0300

    Adiciona métodos ao schedulingRepository para buscar agendamentos por nome, status de pagamento e lembrete enviado

--- src/main/java/com/zentry/whatsappapi/infrastructure/Repository/schedulingRepository.java
+++ src/main/java/com/zentry/whatsappapi/infrastructure/Repository/schedulingRepository.java
@@ -3,9 +3,19 @@ package com.zentry.whatsappapi.infrastructure.Repository;
 import com.zentry.whatsappapi.domain.model.scheduling.scheduling;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface schedulingRepository extends  MongoRepository<scheduling, String> {
 
     scheduling findByDocumento(String documento);
 
     scheduling findByTelefone(String telefone);
+
+    scheduling findByNome(String nome);
+
+    List<scheduling> findByStatusPagamento(String statusPagamento);
+
+    List<scheduling> findByLembreteEnviado(boolean lembreteEnviado);
+
+
 }
